65f8ffd98c9989c944220056553cdbed44b09c51
Kutsan Kaplan <me@kutsankaplan.com>
2016-05-28 00:27:12 +0300

    Fixed some code mistakes

--- src/color.js
+++ src/color.js
@@ -10,15 +10,19 @@ function updateTime()
     var h = checkTime(d.getHours());
     var m = checkTime(d.getMinutes());
     var s = checkTime(d.getSeconds());
-    
+
     document.getElementById("hex").innerHTML = "#" + h + m + s;
     document.body.style.backgroundColor = "#" + h + m + s;
 
-    setTimeout(function() { updateTime(); }, 1000);
+    setTimeout(function ()
+    {
+        updateTime();
+    }, 1000);
 }
 
-document.body.onload = function() {
-    updateTime()();
+document.body.onload = function ()
+{
+    updateTime();
 };
 
 function checkTime(i)
